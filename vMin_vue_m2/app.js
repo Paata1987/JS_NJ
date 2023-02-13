3530d3aff28b70cf37f3dbfc65cdd8d41ac700d6
unknown <paatatepnadze1@gmail.com>
2023-02-13 13:52:59 +0100

    binding via v-model/ add computed and watch examples

--- vMin_vue_m2/app.js
+++ vMin_vue_m2/app.js
@@ -8,12 +8,33 @@ const App = {
     };
   },
   methods: {
-    inputCHangeHandler(event) {
-      //console.log(event.target.value);
-      this.inputValue = event.target.value;
-    },
     addNewNote() {
-      this.notes.push(this.inputValue);
+      if (this.inputValue !== "") {
+        this.notes.push(this.inputValue);
+        this.inputValue = "";
+      }
+    },
+    toUpperCase(item) {
+      return item.toUpperCase();
+    },
+
+    removeNote(idx) {
+      this.notes.splice(idx, 1);
+    },
+  },
+  computed: {
+    doubleCountCompiuted() {
+      console.log("compiutedLog: ");
+
+      return this.notes.length * 3;
+    },
+  },
+  watch: {
+    inputValue(value) {
+      if (value.length > 10) {
+        this.inputValue = "";
+      }
+      console.log("changed value", value);
     },
   },
 };
