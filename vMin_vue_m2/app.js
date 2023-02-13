8d77093b2b49468f4ee36a06d131626228d17c9d
unknown <paatatepnadze1@gmail.com>
2023-02-13 11:11:27 +0100

    binding methods input interpolation. exaples

--- vMin_vue_m2/app.js
+++ vMin_vue_m2/app.js
@@ -1,9 +1,20 @@
 const App = {
   data() {
     return {
-      counter: 0,
-      title: "counter",
+      title: "Note List",
+      placeholderString: "task name",
+      inputValue: "",
+      notes: ["something1", "something2", "something3"],
     };
+  },
+  methods: {
+    inputCHangeHandler(event) {
+      //console.log(event.target.value);
+      this.inputValue = event.target.value;
+    },
+    addNewNote() {
+      this.notes.push(this.inputValue);
+    },
   },
 };
 Vue.createApp(App).mount("#app");
